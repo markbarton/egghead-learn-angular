bed1f4d01e2ecf9a49e7fe6bb7d06bbbc6495dba
Taylor Bell <taylorbell@gmail.com>
2018-06-15 15:22:04 -0600

    prompt and create directory

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 const axios = require('axios')
 const Input = require('prompt-input')
 const lodash = require('lodash')
+const fs = require('fs-extra')
 const {get} = lodash
 
 const http = axios.create()
+
+async function createDirectory (dir) {
+  try {
+    await fs.ensureDir(`output/${dir}`)
+    console.log('Created directory')
+  } catch (err) {
+    console.error(err)
+  }
+}
 
 const makeRequest = (slug) => {
   const reqUrl = `https://egghead.io/api/v1/series/${slug}?load_lessons=true`
   
   http.get(reqUrl)
     .then(({data}) => {
-      console.log(get(data, 'title'))
+      console.log(`Course Found! ${get(data, 'title')}`)
+      createDirectory(get(data, 'slug')) 
     })
     .catch(error => {
       console.log('Error!')
