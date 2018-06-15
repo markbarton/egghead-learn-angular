07578ce3acb7bd30b4f239dd8eaf9c89eaa27125
Taylor Bell <taylorbell@gmail.com>
2018-06-15 15:54:58 -0600

    generate readme

--- src/index.js
+++ src/index.js
@@ -9,9 +9,23 @@ const http = axios.create()
 async function createDirectory (dir) {
   try {
     await fs.ensureDir(`output/${dir}`)
+    await fs.ensureDir(`output/${dir}/images`)
+    await fs.ensureDir(`output/${dir}/lessons`)
     console.log('Created directory')
   } catch (err) {
     console.error(err)
+  }
+}
+
+async function createReadme (slug, title, image, description, instructor, path) {
+  const readme = `# ${title}\n\n![Course Image](${image})\n\nAsciicasts for [${instructor.full_name}](${instructor.instructor_url})'s course, ${title} on [egghead.io](https://egghead.io/${path})\n\n## Description\n${description}\n\n## Library Version\n`
+  
+  const file = `output/${slug}/README.md`
+
+  try {
+    await fs.outputFile(file, readme)
+  } catch (err) {
+    console.error('Error creating file!')
   }
 }
 
@@ -20,8 +34,12 @@ const makeRequest = (slug) => {
   
   http.get(reqUrl)
     .then(({data}) => {
+      const {slug, title, author, description, square_cover_url, instructor, path} = data
       console.log(`Course Found! ${get(data, 'title')}`)
-      createDirectory(get(data, 'slug')) 
+      
+      createDirectory(slug)
+      createReadme(slug, title, square_cover_url, description, instructor, path)
+
     })
     .catch(error => {
       console.log('Error!')
